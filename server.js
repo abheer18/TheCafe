c18e69d609acf51e5417dccea70210f78197e292
ABHEER MEHROTRA <72267713+abheer18@users.noreply.github.com>
2021-11-15 23:15:23 +0530

    edited server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;   //edited this port
 
 const user = "fitnessblog223@gmail.com";
 const pass = "@everyonecanuse";
