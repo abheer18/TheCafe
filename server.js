8a7a3951e06d68cc2d5c5ada62987745e3766f21
Sanchit <sanchit02@outlook.com>
2021-11-27 12:39:17 +0530

    fix all bugs

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
+require("dotenv").config(".env");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;   //edited this port
-// const port = process.env.USER || 8080;
-// const port = process.env.PASSWORD || 8080;
+const port = process.env.PORT || 3000; //edited this port
+
 const user = process.env.USER;
 const pass = process.env.PASS;
 
@@ -14,69 +13,72 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const connection = require('./database/db');
+const connection = require("./database/db");
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log("Database connected")
-})
+  console.log("Database connected");
+});
 
 app.post("/send", (req, res) => {
   let transporter = nodemailer.createTransport({
     service: "gmail",
-    port: 465,
-    secure: false,
     auth: {
       user: user,
-      password: pass,
-    },
-    tls: {
-      rejectUnauthorized: false,
+      pass: pass,
     },
   });
-  var x = req.body
-console.log(req.body)
-  const SQL = "INSERT INTO `feedback` SET `NAME`=?,`EMAILID`=?,`SUBJECT`=?,`MESSAGE`=?";
+  var x = req.body;
+  // console.log(req.body);
+  const SQL =
+    "INSERT INTO `feedback` SET `NAME`=?, `EMAILID`=?, `SUBJECT`=?, `MESSAGE`=? ";
 
-  const query = connection.query(SQL, [x.NAME,x.EMAILID,x.SUBJECT,x.MESSAGE], (err, output) => {
-    if (err) {
-      // throw err;
-      console.log(err.message)
-    } else {
-      res.send("Query run successfully");
+  const query = connection.query(
+    SQL,
+    [x.NAME, x.EMAILID, x.SUBJECT, x.MESSAGE],
+    (err, output) => {
+      if (err) {
+        // throw err;
+        console.log(err.message);
+      } else {
+        res.status(200).json({
+          dbMessage: "Query Run successfully",
+        });
+
+        let mailOptions = {
+          from: user,
+          to: req.body.EMAILID,
+          subject: req.body.SUBJECT,
+          text: `name: ${req.body.NAME} 
+          email: ${req.body.EMAILID} 
+          message: ${req.body.MESSAGE}
+          `,
+        };
+
+        /*
+        `name: ${req.body.name}
+          email: ${req.body.email}
+          message: ${req.body.message}
+          `,
+        */
+
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+            res.status(200).json({
+              emailMessage: `message send ${JSON.stringify(req.body)}`,
+            });
+          }
+        });
+      }
     }
-  } )
-
-  let mailOptions = {
-    from: user,
-    to: req.body.email,
-    subject: req.body.subject,
-    text: `name: ${req.body.name}
-email: ${req.body.email}
-message: ${req.body.message}
-    `,
-  };
-
-  /*
-  `name: ${req.body.name}
-    email: ${req.body.email}
-    message: ${req.body.message}
-    `,
-  */
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
-
-  // res.send(`message send ${JSON.stringify(req.body)}`);
+  );
 });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`App running on http://localhost:${port}`);
 });
 
 // NAME, EMAILID, SUBJECT ,MESSAGE
